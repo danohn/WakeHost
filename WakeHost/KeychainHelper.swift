cbb106da2c94fe0f7c4eaced6e321af3a0c50356
danohn <82357071+danohn@users.noreply.github.com>
2026-03-24 17:46:49 +1100

    Refactor KeychainHelper to simplify query methods and remove unused data protection logic

--- WakeHost/KeychainHelper.swift
+++ WakeHost/KeychainHelper.swift
@@ -4,23 +4,24 @@ import Security
 /// A simple helper for storing/retrieving strings in the keychain.
 struct KeychainHelper {
     private static let service = "com.dohnesorge.WakeHost"
-    private static let useDataProtectionKeychain = true
-    private static let itemAccessibility = kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
 
     static func set(_ value: String, forKey key: String) {
         guard let data = value.data(using: .utf8) else { return }
 
-        SecItemDelete(scopedQuery(for: key) as CFDictionary)
+        SecItemDelete(query(for: key) as CFDictionary)
 
-        var attributes = scopedQuery(for: key)
-        attributes[kSecValueData as String] = data
-        attributes[kSecAttrAccessible as String] = itemAccessibility
+        let attributes: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ]
 
         SecItemAdd(attributes as CFDictionary, nil)
     }
 
     static func get(_ key: String) -> String? {
-        let query = scopedQuery(for: key, returningData: true)
+        let query = query(for: key, returningData: true)
         var dataTypeRef: AnyObject?
         let status = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
 
@@ -32,18 +33,17 @@ struct KeychainHelper {
     }
 
     static func delete(_ key: String) {
-        SecItemDelete(scopedQuery(for: key) as CFDictionary)
+        SecItemDelete(query(for: key) as CFDictionary)
     }
 
-    private static func scopedQuery(
+    private static func query(
         for key: String,
         returningData: Bool = false
     ) -> [String: Any] {
         var query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrService as String: service,
-            kSecAttrAccount as String: key,
-            kSecUseDataProtectionKeychain as String: useDataProtectionKeychain
+            kSecAttrAccount as String: key
         ]
 
         if returningData {
